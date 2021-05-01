069d0d0015584dc771c0993a28c06519272c40c6
Caitlin <caitlin.grange94@gmail.com>
2021-05-01 17:24:34 +1000

    added clear brackets program

---
+++ Edabit/ClearBrackets/ClearBrackets.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+/// <summary>
+/// Create a function Brackets() that takes a string and checks that the brackets in the math expression are correct.
+/// The function should return true or false.
+///
+/// Examples
+/// Brackets("(a*(b-c)..... )") ➞ true
+/// Brackets(")(a-b-45/7*(a-34))") ➞ false
+/// Brackets("sin(90...)+.............cos1)") ➞ false
+///
+/// Notes
+/// The string may not contain brackets, then return true.
+/// String may contain spaces.
+/// The string may be empty.
+///
+/// </summary>
+
+namespace ClearBrackets
+{
+	class ClearBrackets
+	{
+		public static bool Brackets(string str)
+		{
+			var balanced = 0;
+
+			foreach (var c in str)
+			{
+				_ = c.Equals('(') ? balanced++ : balanced;
+				_ = c.Equals(')') ? balanced-- : balanced;
+
+				if (balanced < 0)
+				{
+					return false;
+				}
+			}
+
+			return balanced == 0;
+		}
+	}
+}
---
+++ Edabit/ClearBrackets/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ClearBrackets
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+
+			Console.WriteLine(ClearBrackets.Brackets("(a*(b-c)     )"));
+			Console.WriteLine(ClearBrackets.Brackets(" ) (a-b-45/7*(a-34))"));
+			Console.WriteLine(ClearBrackets.Brackets("sin(90       )+      cos1)"));
+			Console.WriteLine(ClearBrackets.Brackets(" (...). .%_.(.... )"));
+			Console.WriteLine(ClearBrackets.Brackets(" (...)...(..(...).... )  "));
+			Console.WriteLine(ClearBrackets.Brackets(") .. .() (        ).. . ("));
+			Console.WriteLine(ClearBrackets.Brackets(")))((("));
+			Console.WriteLine(ClearBrackets.Brackets("  (...).!.)...("));
+			Console.WriteLine(ClearBrackets.Brackets("a+b"));
+			Console.WriteLine(ClearBrackets.Brackets(""));
+			Console.WriteLine(ClearBrackets.Brackets("(a+f).`!£=.)...) "));
+		}
+	}
+}
