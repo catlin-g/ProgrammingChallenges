df9add5dbadb4b5bfb27346ce2aff000272ec919
Caitlin <caitlin.grange94@gmail.com>
2021-07-15 16:26:19 +1000

    added solution to concatenationofarrays

--- Leetcode/SolutionConcatenationOfArray.cs
+++ Leetcode/SolutionConcatenationOfArray.cs
@@ -1,13 +1,19 @@
-﻿using System;
-
-namespace LeetCode
+﻿namespace LeetCode
 {
 	public class SolutionConcatenationOfArray
 	{
 		public int[] GetConcatenation(int[] nums)
 		{
+			var numsLength = nums.Length;
+			var result = new int[numsLength * 2];
 
-			return null;
+			for (var i = 0; i < numsLength; ++i)
+			{
+				result[i] = nums[i];
+				result[i + numsLength] = nums[i];
+			}
+
+			return result;
 		}
 	}
 }
